9b31ce3627553e464b0c6846a3ba46432b45c3d1
Moojuiceman <moojuiceman@hotmail.co.uk>
2018-11-18 01:47:00 +0000

    First working version

---
+++ SortOrderParts/Main.cs
@@ -0,0 +1,91 @@
+﻿using Harmony12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityModManagerNet;
+using Utils;
+
+namespace SortOrderPartsNS
+{
+    public static class SortOrderParts
+    {
+        static int currentProfile = 0;
+        static List<int> sortedJobs = new List<int>();
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            if (modEntry.Enabled)
+            {
+                UMMLogger.Init(modEntry.Path + "/Log.txt", "Starting");
+
+                HarmonyInstance harmony = HarmonyInstance.Create(modEntry.Info.Id);
+
+                harmony.PatchAll();
+
+                UMMLogger.WriteLog("Patched");
+            }
+
+            return true;
+        }
+
+        internal static void SortJob(ref Job job, CarLoader carLoader)
+        {
+            if (currentProfile != ProfileManager.Get().GetSelectedProfile())
+            {
+                currentProfile = ProfileManager.Get().GetSelectedProfile();
+                sortedJobs.Clear();
+            }
+
+            if (!sortedJobs.Contains(job.id))
+            {
+                foreach (JobPart jobPart in job.jobParts)
+                {
+                    Dictionary<string, int> partDict = new Dictionary<string, int>();
+
+                    //Store the existing order
+                    for (int i = 0; i < jobPart.partsCount; i++)
+                    {
+                        partDict.Add(jobPart.partList[i], i);
+                    }
+
+                    List<string> sorted = new List<string>();
+
+                    //Sort them by the display name
+                    if (jobPart.type != "Body")
+                    {
+                        sorted = (from p in jobPart.partList
+                                  orderby GameInventory.Get().GetItemLocalizeName(p.Substring(p.LastIndexOf('/') + 1))
+                                  select p).ToList();
+                    }
+                    else
+                    {
+                        sorted = (from p in jobPart.partList
+                                  orderby GameInventory.Get().GetBodyLocalizedName(carLoader.carToLoad + "-" + carLoader.GetDefaultName(p.Substring(p.LastIndexOf('/') + 1)))
+                                  select p).ToList();
+                    }
+
+                    jobPart.partList = sorted;
+                }
+
+                sortedJobs.Add(job.id);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(OrderGenerator), "GetJobForCarLoader")]
+    static class OrderGenerator_GetJobForCarLoader_Patch
+    {
+        static void Postfix(ref Job __result, int carLoaderID)
+        {
+            try
+            {
+                SortOrderParts.SortJob(ref __result, CarLoaderPlaces.Get().GetCarLoaderByIndex(carLoaderID));
+            }
+            catch (Exception e)
+            {
+                UMMLogger.LogException(e);
+            }
+        }
+    }
+}
---
+++ SortOrderParts/Utils.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Utils
+{
+    internal static class Utils
+    {
+        public static string Left(this string s, int count)
+        {
+            return s.Substring(0, count);
+        }
+
+        public static string Right(this string s, int count)
+        {
+            return s.Substring(s.Length - count);
+        }
+    }
+
+    internal static class UMMLogger
+    {
+        internal static string LogPath { get; private set; }
+        internal static bool LoggingOk { get; private set; }
+
+        internal static void Init(string path, string text)
+        {
+            LogPath = path;
+            LoggingOk = true;
+            try
+            {
+                WriteLog(text);
+            }
+            catch (Exception e)
+            {
+                LoggingOk = false;
+                throw e;
+            }
+        }
+
+        internal static void WriteLog(string text)
+        {
+            if (LoggingOk)
+            {
+                File.AppendAllText(LogPath, "[" + DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Unspecified).ToString("yyyy-MM-dd HH:mm:ss") + "] " + text + "\n");
+            }
+        }
+
+        internal static void LogException(Exception e)
+        {
+            string message = "Exception info: \n" + e.Message + "\n";
+            if (e.InnerException != null)
+            {
+                message += e.InnerException.Message + "\n";
+            }
+            message += e.StackTrace;
+            WriteLog(message);
+        }
+    }
+}
